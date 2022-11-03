adfa8e695de90188e9d141252fcbc85dde00ab4b
Jassig98 <114430075+Jassig98@users.noreply.github.com>
2022-11-02 23:10:30 -0700

    Update add_surname.py

--- add_surname.py
+++ add_surname.py
@@ -4,6 +4,6 @@
 # Description: Program that takes list of first names and returns only the names that start with K and add surname Kardashian
 
 def add_surname(first_names_list):
-    full_list= [name+ 'Kardashian' for name in first_names_list if name [0] == 'K']
+    full_list= [name + 'Kardashian' for name in first_names_list if name [0] == 'K']
     return full_list
 
